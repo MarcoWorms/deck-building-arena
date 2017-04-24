33d1bb2bca025aec2b435e997ed07cd66f99e6cf
Marco Worms <marcogworms@gmail.com>
2017-04-24 00:04:57 -0300

    buildDeck: add boilerplate code

---
+++ src/game/buildDeck/index.js
@@ -0,0 +1,17 @@
+const {
+  pipe,
+  repeat,
+  reduce,
+  toPairs,
+} = require('ramda')
+
+module.exports = pipe(
+  toPairs,
+  reduce(
+    (deck, [name, amount]) => [
+      ...deck,
+      ...repeat(name, amount)
+    ],
+    []
+  )
+)
