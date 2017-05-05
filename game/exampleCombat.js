06ca719b9c8c17e28615d448f87b732456c3fa1c
Marco Worms <marcogworms@gmail.com>
2017-05-05 11:29:45 -0300

    combat: improve turns example and add names to cards

--- game/exampleCombat.js
+++ game/exampleCombat.js
@@ -18,13 +18,64 @@ export default {
   turns: [
     {
       index: 0,
-      playersLife: [50, 50],
-      fields: [],
+      fields: [[], []],
     },
     {
       index: 1,
-      playersLife: [50, 50],
-      fields: [],
+      fields: [
+        [
+          {
+            id: 'apian-jet-coot',
+            attacks: [],
+          },
+        ],
+        [
+          {
+            id: 'murderous-pixyish-neontetra',
+            attacks: [
+              {
+                target: 'player',
+                damage: 1,
+              },
+            ],
+          },
+        ],
+      ],
+    },
+    {
+      index: 2,
+      fields: [
+        [
+          {
+            id: 'apian-jet-coot',
+            attacks: [
+              {
+                target: 'player',
+                damage: 2,
+              },
+            ],
+          },
+          {
+            id: 'changeful-durational-rhesusmonkey',
+            attacks: [],
+          },
+        ],
+        [
+          {
+            id: 'murderous-pixyish-neontetra',
+            attacks: [
+              {
+                target: 'player',
+                damage: 1,
+              },
+            ],
+          },
+          {
+            id: 'uncolourable-cubic-muntjac',
+            attacks: [],
+          },
+        ],
+      ],
     },
   ],
 }
--- game/models/cards.js
+++ game/models/cards.js
@@ -1,36 +1,42 @@
 export default [
 // Turn 1
   {
+    id: 'apian-jet-coot',
     turn: 1,
     attack: 2,
     defense: 2,
     traits: [],
   },
   {
+    id: 'murderous-pixyish-neontetra',
     turn: 1,
     attack: 1,
     defense: 1,
     traits: ['quick'],
   },
   {
+    id: 'queenlike-fruitarian-blackcrappie',
     turn: 1,
     attack: 1,
     defense: 1,
     traits: ['splash'],
   },
   {
+    id: 'craniological-unworldly-argentinehornedfrog',
     turn: 1,
     attack: 2,
     defense: 1,
     traits: ['tank'],
   },
   {
+    id: 'unconstant-rainbow-eskimodog',
     turn: 1,
     attack: 1,
     defense: 1,
     traits: ['shield'],
   },
   {
+    id: 'verified-unoutlawed-ichidna',
     turn: 1,
     attack: 2,
     defense: 1,
@@ -38,42 +44,49 @@ export default [
   },
 // Turn 2
   {
+    id: 'changeful-durational-rhesusmonkey',
     turn: 2,
     attack: 2,
     defense: 3,
     traits: [],
   },
   {
+    id: 'powerful-forbidden-chicken',
     turn: 2,
     attack: 3,
     defense: 2,
     traits: [],
   },
   {
+    id: 'chipper-patronizing-cougar',
     turn: 2,
     attack: 2,
     defense: 1,
     traits: ['quick'],
   },
   {
+    id: 'uncolourable-cubic-muntjac',
     turn: 2,
     attack: 1,
     defense: 2,
     traits: ['splash'],
   },
   {
+    id: 'verymad-shortish-stickleback',
     turn: 2,
     attack: 1,
     defense: 3,
     traits: ['tank'],
   },
   {
+    id: 'gluteal-complicated-grizzlybear',
     turn: 2,
     attack: 2,
     defense: 2,
     traits: ['shield'],
   },
   {
+    id: 'unbecoming-quasidepressed-arcticseal',
     turn: 2,
     attack: 3,
     defense: 1,
@@ -81,42 +94,49 @@ export default [
   },
 // Turn 3
   {
+    id: 'leisured-antediluvian-hogget',
     turn: 3,
     attack: 4,
     defense: 3,
     traits: [],
   },
   {
+    id: 'semiparalyzed-charterable-pig',
     turn: 3,
     attack: 3,
     defense: 4,
     traits: [],
   },
   {
+    id: 'entomologic-homespun-earthworm',
     turn: 3,
     attack: 3,
     defense: 2,
     traits: ['quick'],
   },
   {
+    id: 'cork-pyrophobic-newt',
     turn: 3,
     attack: 2,
     defense: 2,
     traits: ['splash'],
   },
   {
+    id: 'flannel-extinct-malamute',
     turn: 3,
     attack: 3,
     defense: 3,
     traits: ['tank'],
   },
   {
+    id: 'snippy-humoursome-ant',
     turn: 3,
     attack: 3,
     defense: 2,
     traits: ['shield'],
   },
   {
+    id: 'dispossessed-growable-peafowl',
     turn: 3,
     attack: 3,
     defense: 2,
@@ -124,42 +144,49 @@ export default [
   },
 // Turn 4
   {
+    id: 'censorable-raptorial-needlefish'
     turn: 4,
     attack: 4,
     defense: 5,
     traits: [],
   },
   {
+    id: 'grandiloquent-impassioned-chameleon'
     turn: 4,
     attack: 5,
     defense: 4,
     traits: [],
   },
   {
+    id: 'communicatory-bratty-cockatiel'
     turn: 4,
     attack: 3,
     defense: 5,
     traits: ['quick'],
   },
   {
+    id: 'needy-pentagonal-monkey'
     turn: 4,
     attack: 2,
     defense: 4,
     traits: ['splash'],
   },
   {
+    id: 'sufficient-consecutive-amurstarfish'
     turn: 4,
     attack: 3,
     defense: 5,
     traits: ['tank'],
   },
   {
+    id: 'nude-intraspinal-hanumanmonkey'
     turn: 4,
     attack: 5,
     defense: 3,
     traits: ['shield'],
   },
   {
+    id: 'expected-nonmetallic-oriole'
     turn: 4,
     attack: 4,
     defense: 2,
@@ -167,36 +194,42 @@ export default [
   },
 // Turn 5
   {
+    id: 'eastern-polkadotted-urva',
     turn: 5,
     attack: 6,
     defense: 6,
     traits: [],
   },
   {
+    id: 'creative-undelighting-dwarfmongoose',
     turn: 5,
     attack: 6,
     defense: 3,
     traits: ['quick'],
   },
   {
+    id: 'featheredged-rusted-shelduck',
     turn: 5,
     attack: 3,
     defense: 5,
     traits: ['splash'],
   },
   {
+    id: 'depositional-adobe-argali',
     turn: 5,
     attack: 3,
     defense: 7,
     traits: ['tank'],
   },
   {
+    id: 'culinary-houndish-anemone',
     turn: 5,
     attack: 6,
     defense: 3,
     traits: ['shield'],
   },
   {
+    id: 'inconvenient-front-dassierat'
     turn: 5,
     attack: 6,
     defense: 2,
